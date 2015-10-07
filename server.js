cfe72dfa066fd4ca22e6e6415604ddab5e170f7e
faithful_dove <david.kates@yale.edu>
2015-10-07 19:10:44 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
-});
+    response.send('Helo MGT-656!');
+})
+
+app.get('/nickname', function(request, response){
+    response.send('faithful-dove');
+})
+
+;
 
 app.listen(process.env.PORT || 4000);
